0b3e1bde4e63321a6d07fb8b670d56f0e6ce34ea
blessymarybiju <138175146+blessymarybiju@users.noreply.github.com>
2024-08-03 23:14:46 -0400

    Update create-vm.ps1

--- create-vm.ps1
+++ create-vm.ps1
@@ -1,15 +1,20 @@
 Connect-AzAccount -UseDeviceAuthentication
-# PowerShell script to create a new Azure VM
 $resourceGroupName = "MyResourceGroup"
 $location = "EastUS"
 $vmName = "MyVM"
 $cred = Get-Credential
+
+# Get the latest Ubuntu image details
 $image = Get-AzVMImage -Location $location -PublisherName Canonical -Offer UbuntuServer -Skus 20.04-LTS -Version latest
 
+# Create a new VM
 New-AzVM `
   -ResourceGroupName $resourceGroupName `
   -Name $vmName `
   -Location $location `
   -Credential $cred `
-  -ImageId $image.Id `
+  -ImagePublisher "Canonical" `
+  -ImageOffer "UbuntuServer" `
+  -ImageSkus "20.04-LTS" `
   -Size "Standard_D2s_v3"
+
