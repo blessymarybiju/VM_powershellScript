305d20a68e26df6e2e2a87362e41d03426c8d1fc
blessymarybiju <138175146+blessymarybiju@users.noreply.github.com>
2024-08-03 23:11:37 -0400

    Update create-vm.ps1

--- create-vm.ps1
+++ create-vm.ps1
@@ -4,10 +4,12 @@ $resourceGroupName = "MyResourceGroup"
 $location = "EastUS"
 $vmName = "MyVM"
 $cred = Get-Credential
+$image = Get-AzVMImage -Location $location -PublisherName Canonical -Offer UbuntuServer -Skus 20.04-LTS -Version latest
+
 New-AzVM `
   -ResourceGroupName $resourceGroupName `
   -Name $vmName `
   -Location $location `
   -Credential $cred `
-  -ImageName "UbuntuLTS"
+  -ImageId $image.Id `
   -Size "Standard_D2s_v3"
